8ddfd8ff0edcd2b5a366b3b4d388511292dcf041
Veniamin Sazonov <harvestnide@gmail.com>
2021-04-28 23:33:56 +0700

    feat: removing dist folder from repo

--- dist/issueOutput.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.issueOutput = void 0;
-var alfy = require('alfy');
-var issueOutput = function () {
-};
-exports.issueOutput = issueOutput;
--- dist/lib/util.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.charTrim = void 0;
-var charTrim = function (item) { return item === null || item === void 0 ? void 0 : item.replace(/\s/gi, ' '); };
-exports.charTrim = charTrim;
